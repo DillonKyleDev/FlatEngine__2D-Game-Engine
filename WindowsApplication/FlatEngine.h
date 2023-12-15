92bb68e2bbc33a0be4ae5129f7ccb8ccc9ae0621
Dillon <dillonkyle95@protonmail.com>
2023-12-14 20:49:45 -0800

    FlatEngine namespace defined

---
+++ WindowsApplication/FlatEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+namespace FlatEngine
+{
+	bool RenderHierarchy();
+};
+
--- WindowsApplication/GameObjectManager.cpp
+++ WindowsApplication/GameObjectManager.cpp
@@ -1,29 +1,27 @@
 #include "GameObjectManager.h"
 
-namespace GameObjectManager
+GameObject GameObjectManager::CreateGameObject(std::string name)
 {
-	GameObject GameObjectManager::CreateGameObject(std::string name)
-	{
-		GameObject newObject;
-		newObject.SetName(name);
-		GameObjectManager::gameObjects.push_back(newObject);
-		return newObject;
-	}
-
-
-	void GameObjectManager::DeleteGameObject(GameObject gameObject)
-	{
-
-	}
-
-
-	void GameObjectManager::IncrementID()
-	{
-		GameObjectManager::currentID += 1;
-	}
-
-
-	//Variables
-	std::vector<GameObject> GameObjectManager::gameObjects;
-	long GameObjectManager::currentID = 0;
+	GameObject newObject;
+	newObject.SetName(name);
+	GameObjectManager::gameObjects.push_back(newObject);
+	return newObject;
 }
+
+
+void GameObjectManager::DeleteGameObject(GameObject gameObject)
+{
+
+}
+
+
+void GameObjectManager::IncrementID()
+{
+	GameObjectManager::currentID += 1;
+}
+
+
+//Variables
+std::vector<GameObject> GameObjectManager::gameObjects;
+long GameObjectManager::currentID = 0;
+
