7031b036fe172d3782388b013c0b3aec4ef86c50
Dillon <dillonkyle95@protonmail.com>
2023-12-15 16:24:22 -0800

    Continuing organization

--- WindowsApplication/FlatEngine.cpp
+++ WindowsApplication/FlatEngine.cpp
@@ -1,34 +1,16 @@
 #include "FlatEngine.h"
-#include <imgui.h>
-#include "GameObjectManager.h"
 
 
-bool FlatEngine::RenderHierarchy()
+/*
+######################################
+######							######
+######         Engine           ######
+######                          ######
+######################################
+*/
+
+
+namespace FlatEngine
 {
-	GameObject newGameObject;
-	Component characterSprite;
 
-
-	newGameObject.SetName("MainCharacter");
-	newGameObject.AddComponent(characterSprite);
-	std::string tempName = newGameObject.GetName();
-	
-	static float f = 0.0f;
-	static int counter = 0;
-
-	ImGui::Begin("Game Object Manager");
-
-	if (ImGui::Button("Create New GameObject"))
-	{
-		GameObjectManager::CreateGameObject("GameObjectTitle");
-	}
-
-	for (int i = 0; i < GameObjectManager::gameObjects.size(); i++)
-	{
-		ImGui::Text("GameObject");
-	}
-
-	ImGui::End();
-
-	return true;
 }
--- WindowsApplication/FlatEngine.h
+++ WindowsApplication/FlatEngine.h
@@ -1,8 +1,42 @@
 #pragma once
+#include <SDL.h>
+#include <SDL_syswm.h>
+#include <imgui.h>
+#include "Window.h"
+#include "GameObjectManager.h"
 
+//ImGui - SDL + DirextX12
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_dx12.h"
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+#ifdef _DEBUG
+#define DX12_ENABLE_DEBUG_LAYER
+#endif
+
+#ifdef DX12_ENABLE_DEBUG_LAYER
+#include <dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
+#endif
 
 namespace FlatEngine
 {
-	bool RenderHierarchy();
+
+	namespace FlatGui
+	{
+		void RenderGui(ImGuiIO& io);
+
+		void SetupImGui(ImGuiIO& io);
+		void HandleEvents(SDL_Event event, bool &quit);
+		void StartFrame();
+		void Render(ImGuiIO& io);
+		void RenderHierarchy();
+		void Cleanup();
+	}
 };
 
